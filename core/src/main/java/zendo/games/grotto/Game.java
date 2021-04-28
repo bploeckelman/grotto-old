800179dfffef9b867f3b71bfaef62d78fdb934f8
Brian Ploeckelman <brian.ploeckelman@gmail.com>
2021-04-28 17:00:31 -0500

    Initial commit

---
+++ core/src/main/java/zendo/games/grotto/Game.java
@@ -0,0 +1,7 @@
+package zendo.games.grotto;
+
+import com.badlogic.gdx.ApplicationAdapter;
+
+/** {@link com.badlogic.gdx.ApplicationListener} implementation shared by all platforms. */
+public class Game extends ApplicationAdapter {
+}
